45be0746ad7274b05c31824a0a320ecaa5ee28e6
Rebecca Mead <beckyleemead@gmail.com>
2020-10-05 17:41:58 -0400

    steps toward drawing grid

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ const board = document.getElementById('board');
 
 // VIEW - render logic
 
+// create the row that will hold the grid squares
+let grid_parent_row = generate_element('div', 'grid_parent_row', 'row h-50 bg-dark', board);
+
 // declare function to generate html elements
-function generate_element(type, id, class_list, parent=false, event=false, event_listener_function=false) {
+function generate_element(type, id, class_list, parent = false, event = false, event_listener_function = false) {
     let new_element = document.createElement(type);
     new_element.id = id;
     new_element.setAttribute('class', class_list);
@@ -17,51 +20,66 @@ function generate_element(type, id, class_list, parent=false, event=false, event
     }
 }
 
-
-// create the parent row that will hold the grid squares
-generate_element("row", "grid_parent_row", "row", board)
-
 // declare class for grid squares
-    // init: generate_element(div, id (number), col-4, grid_parent_row, click_event_listener)
-    // will the states 
+function grid_square(id) {
+    this.id = String(id);
+}
+
+grid_square.prototype.draw = function () {
+    generate_element('div', this.id, 'col-4 h-33', grid_parent_row);
+}
+
+grid_square.prototype.claim = function (player, turn) {
+    let mark = generate_element('h5', player + '-' + turn, '', this);
+    mark.textContent = player;
+}
 
 // MODEL - game logic
 
 // track game_state
-    // initialize to 0
-    // count up with each turn
-    // switch player according to whether game state is even or odd
+// initialize to 0
+// count up with each turn
+// switch player according to whether game state is even or odd
 
 // track board_state
-    // array corresponding to grid tiles:
-        // '' for empty
-        // 'o' for player O
-        // 'x' for player X
-    // initialize to ['', '', '', '', '', '', '', '']
-    // update with each click
+// array corresponding to grid tiles:
+// '' for empty
+// 'o' for player O
+// 'x' for player X
+// initialize to ['', '', '', '', '', '', '', '']
+// update with each click
 
 
 // declare list of all possible winning grid square index combinations
 // let wins = [
-    // [0,1,2],
-    // ...
-    // ]
+// [0,1,2],
+// ...
+// ]
 
 // declare function to check for win or tie
 // function check_board(player)
-    // check all arrays in wins to see if the given indexes in the board_state array match the given player
+// check all arrays in wins to see if the given indexes in the board_state array match the given player
 
 // CONTROLLER
 
 // init (same as reset?)
-    // set game logic to start
-    // load empty board
-    // add event listeners to board tiles
-    // set next player
+function init() {
+    // create the grid squares
+    for (let i = 0; i < 9; i++) {
+        let new_square = new grid_square(i);
+        new_square.draw();
+    }
+}
+// set game logic to start
+// load empty board
+// add event listeners to board tiles
+// set next player
 
 // tile click
-    // update game state
-    // update board_state
-    // when game_state > 5, check win or tie (only for the player that just went)
-        // when relevant: display winner/tie message, lock all tiles, show restart button 
-    // update board & lock tile
+// update game state
+// update board_state
+// when game_state > 5, check win or tie (only for the player that just went)
+// when relevant: display winner/tie message, lock all tiles, show restart button
+// update board & lock tile
+
+init();
