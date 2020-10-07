ba5e39d5eb6fec654bcbbcfa255ea954ca925ed8
Rebecca Mead <beckyleemead@gmail.com>
2020-10-07 11:19:03 -0400

    initial logic to start game and check for win

--- js/main.js
+++ js/main.js
@@ -19,49 +19,59 @@ function generate_element(type, id, class_list, parent = false, event = false, e
 }
 
 // MODEL - game logic
+let game_state, board_state, current_player, winning_combos;
 
-// track game_state
-// initialize to 0
-// count up with each turn
-// switch player according to whether game state is even or odd
-
-// track board_state
-// array corresponding to grid tiles:
-// '' for empty
-// 'o' for player O
-// 'x' for player X
-// initialize to ['', '', '', '', '', '', '', '']
-// update with each click
-
-
-// declare list of all possible winning grid square index combinations
-// let wins = [
-// [0,1,2],
-// ...
-// ]
+// function to initialize game logic
+function start_game() {
+    // track current state of game by counting up number of turns
+    game_state = 0;
+    // track current state of board to check for wins
+    board_state = ['o', 'o', 'o', '', '', '', '', '', ''];
+    current_player = 'x';
+    // declare list of all possible winning grid square index combinations
+}
 
 // declare function to check for win or tie
-// function check_board(player)
-// check all arrays in wins to see if the given indexes in the board_state array match the given player
+function check_game(player) {
+    let winning_combos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    // check all winning combos to see if the given indexes in the board_state array match the given player
+    for (c of winning_combos) {
+        if (c.map((item) => board_state[item]).join('') === player.repeat(3)) {
+            console.log('YAAAAYYY');
+        } else {
+            console.log('NAY')
+        }
+    }
+}
 
 // CONTROLLER
 
 // init (same as reset?)
 function init() {
+    // load empty board
     board.innerHTML = '';
     // create the row that will hold the grid squares
     let grid_parent_row = generate_element('div', 'grid_parent_row', 'row w-50 bg-dark mx-auto mt-3', board);
     // create the grid squares
     for (let i = 0; i < 9; i++) {
-        let new_grid_square = generate_element('div', i, 'col-4', grid_parent_row);
+        // to do: add event listeners to board tiles to trigger tile click function!
+        let new_grid_square = generate_element('div', i, 'col-4 border text-light', grid_parent_row);
         new_grid_square.setAttribute('style', 'height: calc(4em)');
-        let new_paragraph = generate_element('p', 'para_'+i, '', new_grid_square);
+        let new_paragraph = generate_element('p', 'para_' + i, '', new_grid_square);
+        // set game logic to start
+        start_game();
+        // set next player
     }
 }
-// set game logic to start
-// load empty board
-// add event listeners to board tiles
-// set next player
 
 // tile click
 // update game state
@@ -70,4 +80,13 @@ function init() {
 // when relevant: display winner/tie message, lock all tiles, show restart button
 // update board & lock tile
 
+// count up with each turn
+// switch player according to whether game state is even or odd
+
+// '' for empty
+// 'o' for player O
+// 'x' for player X
+// update with each click
 init();
+
+check_game('o');
